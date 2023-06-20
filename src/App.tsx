4b6a267ece9ab6d0754451eacb44d01cb3bde1f5
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-06-21 00:43:02 +0600

    new component rendered

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import ListGroup from './components/ListGroup';
+// import ListGroup from './components/ListGroup';
+
+import LifeCycle from './components/LifeCycle';
+
+// import DataBinding from './components/DataBinding';
 
 function App() {
-  type Item = {
-    id: number;
-    name: string;
-  };
-  const initialItems: Item[] = [
-    { id: 1, name: 'Item 1' },
-    { id: 2, name: 'Item 2' },
-    { id: 3, name: 'Item 3' },
-  ];
+  // type Item = {
+  //   id: number;
+  //   name: string;
+  // };
+  // const initialItems: Item[] = [
+  //   { id: 1, name: 'Item 1' },
+  //   { id: 2, name: 'Item 2' },
+  //   { id: 3, name: 'Item 3' },
+  // ];
   return (
     <div className="flex flex-col justify-center">
-      <ListGroup initialItems={initialItems} heading="Item List" />
+      {/* <ListGroup initialItems={initialItems} /> */}
+
+      {/* <DataBinding /> */}
+
+      <LifeCycle />
     </div>
   );
 }
